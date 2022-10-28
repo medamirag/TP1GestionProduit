04246fe1d711dd473e4ba4af998908e6ac86357a
Amir Agrebi <medamirag@gmail.com>
2022-10-28 10:21:45 +0100

    fixing code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListeProduitsComponent } from './liste-produits/liste-produits.component';
 import { RouterModule } from '@angular/router';
@@ -14,6 +15,7 @@ import { RouterModule } from '@angular/router';
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule,
     RouterModule
   ],
   providers: [],
